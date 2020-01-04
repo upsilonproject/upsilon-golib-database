2e42a397ae239d524dcd8b2d8fa4e9d2186f8565
jamesread <contact@jwread.com>
2020-01-04 01:31:30 +0000

    .

---
+++ pkg/database/backend_sql.go
@@ -0,0 +1,30 @@
+package database;
+
+import (
+	"fmt"
+	"database/sql"
+	"github.com/upsilonproject/upsilon-golib-database/src/models"
+
+)
+
+func PrepareMetricInsert(dbMetrics *sql.DB) (*sql.Stmt) {
+	sql := "INSERT INTO metrics (extracted, updated, serviceCheckResultId, serviceIdentifier, name, value) VALUES (now(), ?, ?, ?, ?, ?)";
+	stmt, err := dbMetrics.Prepare(sql);
+
+	if (err != nil) {
+		panic(err);
+	}
+
+	return stmt;
+}
+
+func insertMetric(stmt *sql.Stmt, result models.ServiceResult, metric models.Metric) {
+	fmt.Println("inserting metric", result.Id, "called", metric.Name, "updated", result.Updated);
+
+	_, err := stmt.Exec(result.Updated, result.Id, result.Identifier, metric.Name, metric.Value)
+
+	if (err != nil) {
+		panic(err);
+	}
+}
+
